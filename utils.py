373c6c2e7f920c2dd6eb48143763a36698268e84
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2024-11-26 11:14:10 +0530

    Added summery and comparator function and reformateed some code

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 # Imports
 import h5py
 import numpy as np
+from tensorflow.keras.layers import *
+from termcolor import colored
 
 
 
@@ -23,3 +25,39 @@ def load_signs_dataset():
 def convert_to_one_hot(Y, C):
     Y = np.eye(C)[Y.reshape(-1)].T
     return Y
+
+
+# extracts the description of a given model
+def summary(model):
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    result = []
+    for layer in model.layers:
+        descriptors = [layer.__class__.__name__, layer.output.shape, layer.count_params()]
+        if (type(layer) == Conv2D):
+            descriptors.append(layer.padding)
+            descriptors.append(layer.activation.__name__)
+            descriptors.append(layer.kernel_initializer.__class__.__name__)
+        if (type(layer) == MaxPooling2D):
+            descriptors.append(layer.pool_size)
+            descriptors.append(layer.strides)
+            descriptors.append(layer.padding)
+        if (type(layer) == Dropout):
+            descriptors.append(layer.rate)
+        if (type(layer) == ZeroPadding2D):
+            descriptors.append(layer.padding)
+        if (type(layer) == Dense):
+            descriptors.append(layer.activation.__name__)
+        result.append(descriptors)
+    return result
+
+# Compare the two inputs
+def comparator(learner, instructor):
+    if learner == instructor:
+        for a, b in zip(learner, instructor):
+            if tuple(a) != tuple(b):
+                print(colored("Test failed", attrs=['bold']), "\n Expected value \n\n", colored(f"{b}", "green"), "\n\n does not match the input value: \n\n", colored(f"{a}", "red"))
+                raise AssertionError("Error in test") 
+        print(colored("All tests passed!", "green"))
+        
+    else:
+        print(colored("Test failed. Your output is not as expected output.", "red"))
