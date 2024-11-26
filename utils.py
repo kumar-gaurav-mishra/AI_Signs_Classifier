28e79a7e5007e230a7cb542cde7f55297b118c80
Kumar Gaurav Mishra <kumargauravmishra2018@gmail.com>
2024-11-26 10:54:45 +0530

    Added function convert to one hot in utils

--- utils.py
+++ utils.py
@@ -19,3 +19,7 @@ def load_signs_dataset():
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+def convert_to_one_hot(Y, C):
+    Y = np.eye(C)[Y.reshape(-1)].T
+    return Y
